59d718d24e0f3790e7d1372683bc8e97fbd5cc9e
Yeltsin <yeltsinbaquedano@gmail.com>
2020-09-27 23:10:11 -0600

    Se agregaron nuevos modelos

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -7,7 +7,7 @@ importlib.reload(sys)
 
 # Create your models here.
 
-class Tipo_producto(models.Model):
+class TipoProducto(models.Model):
 	descripcion_tipo_producto = models.CharField(max_length=100)
 	
 	def __str__(self):
@@ -17,10 +17,28 @@ class Tipo_producto(models.Model):
 
 class Categoria(models.Model):
 	"""docstring for Categoria"""
-	categoria = models.CharField(max_length=50)
+	descripcion_categoria = models.CharField(max_length=50)
 	def __str__(self):
-		return "{}".format(self.categoria)
+		return "{}".format(self.pk,self.descripcion_categoria)
 	#si pertenece a Tecnologia, Hogar, Juguetes
+
+class Marca(models.Model):
+	"""docstring for Marca"""
+	descripcion_marca = models.CharField(max_length=50)
+	def __str__(self):
+		return "{}".format(self.pk,self.descripcion_marca)
+
+class Modelo(models.Model):
+	"""docstring for Modelo"""
+	descripcion_modelo = models.CharField(max_length=50)
+	def __str__(self):
+		return "{}".format(self.pk,self.descripcion_modelo)
+
+class MarcaModelo(models.Model):
+	"""docstring for Modelo"""
+	descripcion_marca_modelo = models.CharField(max_length=50)
+	def __str__(self):
+		return "{}".format(self.pk,self.descripcion_marca_modelo)
 
 class TipoInventario(models.Model):
 	tipo_inventario = models.CharField(max_length=200,blank=True,null=True)
@@ -47,26 +65,38 @@ class Empresa(models.Model):
 	def __str__(self):
 		return "{} {} " .format(self.nombre,self.contacto)
 
-
 class Producto(models.Model):
 	imagen_producto = models.ImageField(upload_to='imagen_producto')
 	nombre_producto = models.CharField(max_length=200,blank=True,null=True)
 	descripcion_producto = models.CharField(max_length=500,blank=True,null=True)
 	fecha_registro = models.DateField(auto_now_add=True,blank=True,null=True)
-	existencia = models.IntegerField(default=0,blank = True, null = True)
 	precio = models.FloatField(default=0,blank = True, null = True)
 	porcentaje_descuento = models.IntegerField(default=0,blank = True, null = True)
 	esta_descuento = models.BooleanField(default = False)
 	nuevo_producto = models.BooleanField(default = False)
-	tipo_producto = models.ForeignKey(Tipo_producto, on_delete=models.CASCADE)
-	tipo_inventario = models.ForeignKey(TipoInventario,blank = True, null = True, on_delete=models.CASCADE)
+	proveedor = models.CharField(max_length=500,blank=True,null=True)
+	tipo_producto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
 	categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+	marca_modelo = models.ForeignKey(MarcaModelo, on_delete=models.CASCADE)
 
 	def __str__(self):
 		return "{}-{} " .format(self.pk,self.nombre_producto,self.categoria)
 
+class Carrito(models.Model):
+	"""docstring for lote"""
+	cantidad = models.CharField(max_length=50)
+	producto =  models.ForeignKey(Producto, on_delete=models.CASCADE)
+	usuario = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
 
+	def __str__(self):
+		return "{}".format(self.pk,self.cantidad,self.producto)
 
+class Lote(models.Model):
+	"""docstring for lote"""
+	existencia = models.CharField(max_length=50)
+	producto =  models.ForeignKey(Producto, on_delete=models.CASCADE)
+	def __str__(self):
+		return "{}".format(self.pk,self.existencia,self.producto)
 #opcional,  dejar en los datos de la empresa
 class Redes_sociales(models.Model):
 	url_facebook = models.CharField(max_length=100,blank=True,null=True)
