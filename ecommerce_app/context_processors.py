f532a26b6513912920ff2790fefffc1e15a05cfb
Yeltsin <yeltsinbaquedano@gmail.com>
2020-12-06 17:57:42 -0600

    Se agrego data al context processor, se agrego url de navegacion de usuario en cada producto

--- ecommerce_app/context_processors.py
+++ ecommerce_app/context_processors.py
@@ -4,5 +4,7 @@ from ecommerce_app.models import *
 def ctx_base_cliente(request):
 	categorias = Categoria.objects.all()
 	empresa = Empresa.objects.get(pk=1)
+	productos = Producto.objects.filter(esta_descuento = True).order_by('-id')[:8]
 	return {'ctx_categorias':categorias,
-			'ctx_empresa':empresa}
+			'ctx_empresa':empresa,
+			'ctx_productos_descuento':productos}
