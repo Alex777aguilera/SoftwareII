a2d85b1f66e6adc082b32198c1416ffa8c7b851c
Yeltsin <yeltsinbaquedano@gmail.com>
2020-12-08 01:19:55 -0600

    Cambio de titulo en pestaña de ecommerce a easy-buy

--- ecommerce_app/context_processors.py
+++ ecommerce_app/context_processors.py
@@ -3,8 +3,10 @@ from ecommerce_app.models import *
 
 def ctx_base_cliente(request):
 	categorias = Categoria.objects.all()
+	subcategorias = SubCategoria.objects.filter().order_by('-id')[:4]
 	empresa = Empresa.objects.get(pk=1)
 	productos = Producto.objects.filter(esta_descuento = True).order_by('-id')[:8]
 	return {'ctx_categorias':categorias,
 			'ctx_empresa':empresa,
-			'ctx_productos_descuento':productos}
+			'ctx_productos_descuento':productos,
+			'ctx_subcategorias':subcategorias}
