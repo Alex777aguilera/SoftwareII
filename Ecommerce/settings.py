a3e86676f3cf2798e1501b0136a31c71855686f9
Yeltsin <yeltsinbaquedano@gmail.com>
2020-12-11 02:53:37 -0600

    Debug True

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2e=5*7jtu(poyd&(yj89fc!7%w(tg)3$1iu*k(=i39=d2&%jcv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+from decouple import config
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['easy-buyuth.herokuapp.com']
 
